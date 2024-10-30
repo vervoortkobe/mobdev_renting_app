af0417997e60fd10c450cd69a4af7120fa918a79
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-10-30 12:56:00 +0100

    Add logout button

--- app/src/main/java/edu/ap/mobiledevrentingapp/MainActivity.kt
+++ app/src/main/java/edu/ap/mobiledevrentingapp/MainActivity.kt
@@ -4,11 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuth
 import edu.ap.mobiledevrentingapp.ui.theme.MobileDevRentingAppTheme
 
@@ -32,7 +42,13 @@ class MainActivity : ComponentActivity() {
                 ) {
                     if (currentUser != null) {
                         // User is logged in, display main app content
-                        Text(text = "Welcome, ${currentUser.email}!")
+                        Column(modifier = Modifier.fillMaxSize()) {
+                            Text(
+                                text = "Welcome, ${currentUser.email}!",
+                                modifier = Modifier.padding(16.dp)
+                            )
+                            LogoutButton(onClick = { signOut() })
+                        }
                     } else {
                         // User is not logged in, redirect to LoginActivity
                         val intent = Intent(this, LoginActivity::class.java)
@@ -42,5 +58,31 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
+
+@Composable
+fun LogoutButton(onClick: () -> Unit) {
+    OutlinedButton(
+        onClick = onClick,
+        content = {
+            Icon(Icons.Filled.ExitToApp, contentDescription = "Logout")
+            Text(text = "Log out")
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MobileDevRentingAppTheme {
+        LogoutButton {}
     }
 }
