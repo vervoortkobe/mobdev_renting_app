40e635f455947e2b0c4dee2893beb24b113e4979
Droopyke <hatchi1411@gmail.com>
2024-11-27 09:40:35 +0100

    Feat: Home Page White

--- app/src/main/java/edu/ap/mobiledevrentingapp/home/HomePage.kt
+++ app/src/main/java/edu/ap/mobiledevrentingapp/home/HomePage.kt
@@ -1,6 +1,8 @@
 package edu.ap.mobiledevrentingapp.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,25 +16,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomePage(onLogout: () -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Welcome to Home!")
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedButton(
-            onClick = onLogout,
-            content = {
-                Icon(Icons.AutoMirrored.Filled.ExitToApp, contentDescription = "Logout")
-                Text(text = "Log out")
-            }
-        )
+    Box(
+        modifier = Modifier.background(Color.White)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Welcome to Home!", color = Color.Black)
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedButton(
+                onClick = onLogout,
+                content = {
+                    Icon(Icons.AutoMirrored.Filled.ExitToApp, contentDescription = "Logout", tint = Color.Black)
+                    Text(text = "Log out", color = Color.Black)
+                }
+            )
+        }
     }
 }
